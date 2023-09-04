1882a8fa343fa528dc4457f25be284a99ae9497a
Alexander J. Vincent <ajvincent@gmail.com>
2023-09-04 11:24:56 -0700

    Packaging for release, part 1.

---
+++ exports.d.ts
@@ -0,0 +1,21 @@
+export MultiMixinBuilder from "./MultiMixinBuilder.js";
+
+export type {
+  MixinClass
+} from "./types/MixinClass.js";
+
+export type {
+  StaticAndInstance
+} from "./types/StaticAndInstance.js";
+
+export type {
+  SubclassDecorator,
+} from "./types/SubclassDecorator.js";
+
+export type {
+  ClassDecoratorFunction
+} from "./types/ClassDecoratorFunction.js";
+
+export type {
+  MultiMixinClass
+} from "./types/MultiMixinClass.js";
